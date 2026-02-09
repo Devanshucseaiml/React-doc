aea514bc1cf0a97f77bf926c24e228a79130fb75
Devanshucseaiml <121437589+Devanshucseaiml@users.noreply.github.com>
2026-02-09 17:25:55 +0000

    Add 01doc: basic component output working

---
+++ react-docs/src/App.jsx
@@ -0,0 +1,9 @@
+import { docsMap } from "./docs";
+
+function App() {
+  const CurrentDoc = docsMap.doc1;
+  return <CurrentDoc />;
+}
+
+export default App;
+
---
+++ react-docs/src/docs/index.js
@@ -0,0 +1,7 @@
+import Doc01 from "./01doc"
+
+
+export const docsMap = {
+  doc1: Doc01,
+ 
+};
